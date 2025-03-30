d8d09004f1218cc85b2be7fd1e26c304ed8700b8
phoebebright <phoebebright310@gmail.com>
2025-03-30 19:12:11 +0100

    sort all tickets descending by default

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import find_packages, setup
 import sys
 
 
-version = '1.3.1b' #PB version
+version = '1.3.1c' #PB version
 
 
 # Provided as an attribute, so you can append to these instead
